6221792ec3360b343deb5f91a943a0d0f7ad5e64
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-27 19:25:14 -0700

    working back and forth!

--- client.js
+++ client.js
@@ -42,20 +42,49 @@ connectionToServer.on('data', chunk => {
   const parsed = JSON.parse(chunk.toString());
   // const parsed = parseMessage(chunk.toString());
   console.log(parsed);
+
+  if(parsed.code === 3) {
+    setTimeout(() => {
+      const downloadClient = net.createConnection(8080, () => {
+        console.log('I am connected to 8080');
+        const copy = fs.createWriteStream(`/Users/chrispiccaro/clone/${parsed.file}`);
+        downloadClient.on('data', chunk => {
+          const parsed = JSON.parse(chunk);
+          if(parsed.code === 6) {
+            console.log('finished');
+            downloadClient.destroy();
+          } else if(parsed.code === 5) {
+            copy.write(parsed.data, err => {
+              if(err) console.log(err);
+            });
+          }
+        });
+        downloadClient.on('finish', () => {
+          console.log('finished');
+        });
+      });
+    }, 1000);
+  }
+
   if(parsed.code === 4) {
     const server = net.createServer(client => {
       console.log('client connected');
       const read = fs.createReadStream(`/Users/chrispiccaro/clone/${parsed.file}`, options);
       read.on('data', chunk => {
-        client.write(chunk);
+        client.write(JSON.stringify({ code: 5, data: chunk }));
       });
       read.on('close', () => {
         console.log('finished');
+        client.write(JSON.stringify({ code: 6 }));
+        server.close(error => {
+          console.log(error || 'closed');
+        });
       });
     });
     server.listen(PORT, () => {
       console.log('listening on 8080');
     });
   }
+
   rl.prompt();
 });
--- client2.js
+++ client2.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 const readline = require('readline');
 // const parseMessage = require('./parse-message');
 
+const PORT = 8080;
+
+const options = {
+  encoding: 'utf8', 
+  highWaterMark: 16 * 1024 
+};
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -35,18 +42,49 @@ connectionToServer.on('data', chunk => {
   const parsed = JSON.parse(chunk.toString());
   // const parsed = parseMessage(chunk.toString());
   console.log(parsed);
+
   if(parsed.code === 3) {
     setTimeout(() => {
       const downloadClient = net.createConnection(8080, () => {
         console.log('I am connected to 8080');
         const copy = fs.createWriteStream(`/Users/chrispiccaro/clone2/${parsed.file}`);
         downloadClient.on('data', chunk => {
-          copy.write(chunk, err => {
-            if(err) console.log(err);
-          });
+          const parsed = JSON.parse(chunk);
+          if(parsed.code === 6) {
+            console.log('finished');
+            downloadClient.destroy();
+          } else if(parsed.code === 5) {
+            copy.write(parsed.data, err => {
+              if(err) console.log(err);
+            });
+          }
+        });
+        downloadClient.on('finish', () => {
+          console.log('finished');
         });
       });
-    }, 2000);
+    }, 1000);
   }
+
+  if(parsed.code === 4) {
+    const server = net.createServer(client => {
+      console.log('client connected');
+      const read = fs.createReadStream(`/Users/chrispiccaro/clone2/${parsed.file}`, options);
+      read.on('data', chunk => {
+        client.write(JSON.stringify({ code: 5, data: chunk }));
+      });
+      read.on('close', () => {
+        console.log('finished');
+        client.write(JSON.stringify({ code: 6 }));
+        server.close(error => {
+          console.log(error || 'closed');
+        });
+      });
+    });
+    server.listen(PORT, () => {
+      console.log('listening on 8080');
+    });
+  }
+
   rl.prompt();
 });
